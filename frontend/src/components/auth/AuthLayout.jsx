85c58a2e7a944c5c21a83e4426bbaf7153d051ea
Syed Haris Shah <syedharryshah1@gmail.com>
2025-06-26 20:19:19 +0500

    modified AuthLayout

--- frontend/src/components/auth/AuthLayout.jsx
+++ frontend/src/components/auth/AuthLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/Logo.png'
 
 const AuthLayout = () => {
     return (
