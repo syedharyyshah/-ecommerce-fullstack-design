6fa7507b989f9545890e0c1aa8905c5acd31945b
Syed Haris Shah <syedharryshah1@gmail.com>
2025-07-10 14:21:50 +0500

    updated the store

--- frontend/src/store/common-slice/index.js
+++ frontend/src/store/common-slice/index.js
@@ -10,7 +10,7 @@ export const getFeatureImages = createAsyncThunk(
   "/order/getFeatureImages",
   async () => {
     const response = await axios.get(
-      `http://localhost:5000/api/common/feature/get`
+      `https://ecommerce-fullstack-design-xk67.vercel.app/api/common/feature/get`
     );
 
     return response.data;
@@ -21,7 +21,7 @@ export const addFeatureImage = createAsyncThunk(
   "/order/addFeatureImage",
   async (image) => {
     const response = await axios.post(
-      `http://localhost:5000/api/common/feature/add`,
+      `https://ecommerce-fullstack-design-xk67.vercel.app/api/common/feature/add`,
       { image }
     );
 
@@ -33,7 +33,7 @@ export const deleteFeatureImage = createAsyncThunk(
   "/order/deleteFeatureImage",
   async (id) => {
     const response = await axios.delete(
-      `http://localhost:5000/api/common/feature/delete/${id}`
+      `https://ecommerce-fullstack-design-xk67.vercel.app/api/common/feature/delete/${id}`
     );
 
     return response.data;
